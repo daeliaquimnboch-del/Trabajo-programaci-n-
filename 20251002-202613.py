2dc8ef9e84261c0f6ddc0fda8b4cc12516c4e50d
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 20:26:30 -0600

    Add files via upload

---
+++ 20251002-202613.py
@@ -0,0 +1,9 @@
+# Crear un diccionario con frutas y sus precios
+frutas = {
+    "manzana": 2.5,
+    "plátano": 1.2,
+    "naranja": 3.0
+}
+
+# Mostrar el precio de una fruta específica
+print("El precio de la manzana es:", frutas["naranja"])
