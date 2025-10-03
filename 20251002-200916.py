b998d976d8190d6198891c925383e9f906cfa152
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 20:09:36 -0600

    Add files via upload

---
+++ 20251002-200916.py
@@ -0,0 +1,12 @@
+# Lista con 5 números
+numeros = [7, 12, 5, 20, 8]
+
+# Inicializamos la suma en 0
+suma = 0
+
+# Recorremos cada número y lo vamos sumando
+for n in numeros:
+    suma += n
+
+# Mostramos el resultado
+print("La suma de los números es:", suma)
