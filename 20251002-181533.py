43718b21884ee12db25a31a86e76ce759186c782
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 19:19:57 -0600

    Add files via upload

---
+++ 20251002-181533.py
@@ -0,0 +1,8 @@
+# Pedir al usuario que ingrese una letra
+letra = input("Ingresa una letra: D")
+
+# Obtener el código ASCII de la letra
+codigo_ascii = ord(68)
+
+# Mostrar el resultado
+print("El código ASCII de", letra, "es: 68", codigo_ascii)
