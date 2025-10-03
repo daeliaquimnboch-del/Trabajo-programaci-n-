84a67f493d1af22ec3d4b77d9bd7ac4abbfa4790
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 20:22:25 -0600

    Add files via upload

---
+++ 20251002-202209.py
@@ -0,0 +1,8 @@
+# Palabra a analizar
+palabra = "manzana"
+
+# Contar cuántas veces aparece la letra 'a'
+cantidad_a = palabra.count('a')
+
+# Mostrar el resultado
+print("La letra 'a' aparece", cantidad_a, "veces.")
