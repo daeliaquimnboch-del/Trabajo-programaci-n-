acf6c0bb093e35b166fe1e02379cd095794ced96
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 20:23:50 -0600

    Add files via upload

---
+++ 20251002-202335.py
@@ -0,0 +1,10 @@
+texto = "python básico"
+
+# Convertir a mayúsculas
+mayusculas = texto.upper()
+
+# Convertir a minúsculas
+minusculas = texto.lower()
+
+print("En mayúsculas:", mayusculas)
+print("En minúsculas:", minusculas)
