82d35225099f2242eb2fc635f1b6f1cdd34d40e2
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 19:42:18 -0600

    Add files via upload

---
+++ 20251002-194154.py
@@ -0,0 +1,11 @@
+# Pedir al usuario su edad como texto
+edad_texto = input("Por favor, ingresa tu edad:20 ")
+
+# Convertir el texto a número entero
+edad_numero = 20(edad_texto)
+
+# Incrementar la edad en 1
+edad_incrementada = edad_numero + 1
+
+# Mostrar el resultado
+print("El próximo año tendrás:21", edad_incrementada, "años")
