73aed77bdbbfa56c00d954270ca766bbde50437e
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 20:14:30 -0600

    Add files via upload

---
+++ 20251002-201408.py
@@ -0,0 +1,8 @@
+# Lista de calificaciones
+calificaciones = [8.5, 9.0, 7.5, 10.0]
+
+# Calcular el promedio
+promedio = sum(calificaciones) / len(calificaciones)
+
+# Mostrar el resultado
+print("El promedio es:", promedio)
