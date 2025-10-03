1afab7344f978667bf40c72467efaa3bd38e861a
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 20:12:40 -0600

    Add files via upload

---
+++ 20251002-201220.py
@@ -0,0 +1,9 @@
+# Creamos una lista vacía
+cuadrados = []
+
+# Recorremos los números del 1 al 5
+for i in range(1, 6):
+    cuadrados.append(i**2)  # Elevamos al cuadrado y agregamos a la lista
+
+# Mostramos la lista
+print(cuadrados)
