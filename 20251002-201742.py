ac11a81006168a6b0d20365dc07bef8907d09da8
daeliaquimnboch-del <d.a.eliaquimnboch@gmail.com>
2025-10-02 20:18:00 -0600

    Add files via upload

---
+++ 20251002-201742.py
@@ -0,0 +1,9 @@
+# Pedir nombre y apellido al usuario
+nombre = input("Ingresa tu nombre:David ")
+apellido = input("Ingresa tu apellido:Quinteros ")
+
+# Unirlos en una sola cadena
+nombre_completo = nombre + " " + apellido
+
+# Mostrar el resultado
+print("Tu nombre completo es:David Quinteros", nombre_completo)
